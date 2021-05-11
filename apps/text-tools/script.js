4993ce6aaccfc29e3269ab03dbfe47a5f5c8b060
vineet.dubey <vineet.dubey@zscaler.com>
2021-05-11 11:11:07 +0530

    Added html/js for text tools

--- apps/text-tools/script.js
+++ apps/text-tools/script.js
@@ -1,1 +1,16 @@
-window.onload = () => {};
+window.onload = () => {
+  const countCharacterTextArea = document.querySelector('#source-character-counter');
+  const countCharacterResult = document.querySelector('#character-count');
+  const countCharacterButton = document.querySelector('#count-character');
+  const countWordTextArea = document.querySelector('#source-word-counter');
+  const countWordResult = document.querySelector('#word-count');
+  const countWordButton = document.querySelector('#count-word');
+
+  countCharacterButton.onclick = () => {
+    countCharacterResult.value = countCharacterTextArea.value.length;
+  };
+
+  countWordButton.onclick = () => {
+    countWordResult.value = countWordTextArea.value.trim().split(/\s+/).length;
+  };
+};
