50df3a823025c4b278496e97807b661d7d01fd72
vineet.dubey <vineet.dubey@zscaler.com>
2021-05-23 18:30:07 +0530

    added clock

--- apps/v2/clock-epoch/script.js
+++ apps/v2/clock-epoch/script.js
@@ -2,4 +2,21 @@ window.onload = () => {
   setInterval(() => {
     document.querySelector('#epoch-display').innerHTML = Math.floor(Date.now() / 1000);
   }, 1000);
+
+  setInterval(() => {
+    const date = new Date();
+
+    document.querySelector('#time-display-ist').innerHTML = new Intl.DateTimeFormat('en-IN', {
+      timeZone: 'Asia/Kolkata',
+      dateStyle: 'full',
+      timeStyle: 'long',
+      hour12: false,
+    }).format(date);
+    document.querySelector('#time-display-utc').innerHTML = new Intl.DateTimeFormat('en-IN', {
+      timeZone: 'UTC',
+      dateStyle: 'full',
+      timeStyle: 'long',
+      hour12: false,
+    }).format(date);
+  }, 1000);
 };
