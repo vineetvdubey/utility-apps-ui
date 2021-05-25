314ce7b070f72cd534c79c218d556539bbc07faf
vineet.dubey <vineet.dubey@zscaler.com>
2021-05-25 16:18:16 +0530

    updated ip finder app

--- apps/v2/ip-finder/script.js
+++ apps/v2/ip-finder/script.js
@@ -1,1 +1,17 @@
-window.onload = () => {};
+window.onload = () => {
+  const ipFinderService = 'https://api.ipify.org?format=json';
+
+  const updateIP = (ip) => {
+    document.querySelector('#ip-display').innerHTML = ip;
+  };
+
+  const updateIPError = () => {
+    document.querySelector('#ip-display').innerHTML = 'Please try after sometime..';
+  };
+
+  const request = new Request(ipFinderService);
+  fetch(request)
+    .then((res) => res.json())
+    .then((json) => updateIP(json.ip))
+    .catch(() => updateIPError());
+};
