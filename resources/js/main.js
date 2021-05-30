1b06b70842fbb29fdacecf2baa73f393336b9e9c
vineet.dubey <vineet.dubey@zscaler.com>
2021-05-31 00:06:21 +0530

    Added hamburger menu

--- resources/js/main.js
+++ resources/js/main.js
@@ -15,3 +15,22 @@ const openTab = (event, appName) => {
 };
 
 document.querySelector('#defaultOpen').click();
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+const mobileMenu = () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+};
+
+hamburger.addEventListener('click', mobileMenu);
+
+const navLink = document.querySelectorAll('.nav-link');
+
+const closeMenu = () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+};
+
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
---
+++ test.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('.nav-menu');
+
+  const mobileMenu = () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  };
+
+  hamburger.addEventListener('click', mobileMenu);
+
+  const navLink = document.querySelectorAll('.nav-link');
+
+  const closeMenu = () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  };
+
+  navLink.forEach((n) => n.addEventListener('click', closeMenu));
+};
